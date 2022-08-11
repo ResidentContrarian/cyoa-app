538348380d6757cac750d265e3be036af8867c97
ResidentContrarian <gearratiossc@gmail.com>
2022-08-11 16:12:12 -0700

    commit

--- adventure/detrack.svelte
+++ adventure/detrack.svelte
@@ -10,11 +10,13 @@
 
 <h2></h2>
 
-<p>
+<p>So the belief that lying is always wrong - wrong at an act-level, as opposed to wrong at a consequences-level, is a pretty strong stance. Is that stance conditional?
 </p>
 <p>
+	Like, assume (as we often must) a Nazi Germany situation. You know the location of a Jewish family, and a Nazi asks you where they are so he can go round them up and ship them to near-certain doom. Is it still wrong to lie?
 </p>
 <p>
+	Note that what I'm trying to get at here is the situation that justifies lying the most. If you can think of one that does that better, sub it in.
 </p>
 <p>
 </p>
@@ -22,6 +24,6 @@
 </p>
 
 <Exits>
-	<Link to=>Yes, it's ALWAYS</Link>
-	<Link to=></Link>
+	<Link to=detrackwhat>Yes.</Link>
+	<Link to=detrackwhynot>Well, no, not then, obviously.</Link>
 </Exits>
--- adventure/detrackwhat.svelte
+++ adventure/detrackwhat.svelte
@@ -10,18 +10,10 @@
 
 <h2></h2>
 
-<p>
-</p>
-<p>
-</p>
-<p>
-</p>
-<p>
-</p>
-<p>
+<p>Really? This is a nice family. Let's say Nazis are the abstract-concept version of Nazis; there's no reasoning behind what they are doing, no justifications at all besides doing bad things to good people. Is it still wrong to lie to save them?
 </p>
 
 <Exits>
-	<Link to=></Link>
-	<Link to=></Link>
+	<Link to=detrackwhynot>Oops, no, misclick.</Link>
+	<Link to=detrackwhat2>Yes, Still.</Link>
 </Exits>
