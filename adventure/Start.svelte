bea564829b54e0145d57cef1a46efd3fa2807c94
ResidentContrarian <gearratiossc@gmail.com>
2022-08-16 19:56:40 -0700

    a

--- adventure/Start.svelte
+++ adventure/Start.svelte
@@ -21,7 +21,7 @@
 		I'm interested in concepts related honesty and lies. If you are here, you probably are too, even if only a bit. 
 	</p>
 	<p>
-		This is an exercise in defining a conceptual space. I want to give people an opportunity to sort of walk through the exact thoughts they have on lying - what makes it right, what makes it wrong, when they do it - as opposed to actually telling them I think about it. 
+		This is an exercise in defining a conceptual space. I want to give people an opportunity to sort of walk through the exact thoughts they have on lying - what makes it right, what makes it wrong, when they do it - as opposed to actually telling them what I think about it. 
 	</p>
 	<p>
 		I'm going to try to keep judgment from leaking through too much, but I'm sure it will some places. Feel free to mentally eat around that particular patch of mold; again, I'm trying to make a tool for defining your own views on honesty, not mine.
